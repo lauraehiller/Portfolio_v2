0291dd31392e899fc34d1a6f1a86f5ccfabd2759
Laura Hiller <Hillerelaura@gmail.com>
2023-01-09 14:31:30 -0800

    Actually fixed 3D Scene bug

--- src/components/Scene/Scene.js
+++ src/components/Scene/Scene.js
@@ -1,17 +1,17 @@
 import React, {Suspense} from 'react';
 import './Scene.scss';
 import { Canvas  } from '@react-three/fiber';
-//import Computer from './Computer.js';
-import ComputerSimple from './ComputerSimple.js';
-//6C6C6C
+import DesktopComputer from './DesktopComputer.js';
+import MobileComputer from './MobileComputer.js';
+
 const Scene = () => {
     return (
         <div className="canvas-container">
             <Canvas>
                 <pointLight color={[0.564, 0.329, 0.956]} position={[10,10,2]} intensity={3}/>
-                <Suspense fallback={null}>
+                <Suspense fallback={<MobileComputer position={[0,-1.2,1]} rotation-y = {Math.PI * -0.5} />}>
                     {/*<Computer position={[0,-1.2,1]} rotation-y = {Math.PI * -0.5} />*/}
-                    <ComputerSimple position={[0,-1.2,1]} rotation-y = {Math.PI * -0.5} />
+                    <DesktopComputer position={[0,-1.2,1]} rotation-y = {Math.PI * -0.5} />
                 </Suspense> 
             </Canvas>
         </div>
